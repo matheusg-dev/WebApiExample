54eff563891397c4194a92ae403c74705152cb49
Matheus Gomes <matheuzgomes82@gmail.com>
2022-08-10 17:24:42 -0300

    TestController.cs

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,12 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
-[route("test/")]
+[Route("test/")]
 public class TestController : ControllerBase
 {
     [HttpGet]
     public string Get()
     {
         return "Minha Api está rodando!";
+    }
+
+    [HttpGet("quadratica/{x}")]
+    public string Get(int x, int a, int b, int c)
+    {
+        int resultado =  a * x * x + b * x + c;
+        return resultado.ToString();
+    }
+
+    [HttpGet("solucao")]
+    public object Get(double a = 1, double b = 0, double c = 0)
+    {
+        if(a == 0)
+        {
+            return new{
+                status = "fail",
+                message = "delta não pode ser negativo"
+            };
+        }
+    }
+
+    {
+        double delta =  Math.sqrt(b*b-4*a*c);
+        double x1 = (-b + (delta)) / a;   
     }
 }
